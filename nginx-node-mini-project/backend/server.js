a4dcf04f78168ebe19ea1b73c347a38facc1799f
vireshmustapure <107835343+vireshmustapure@users.noreply.github.com>
2026-02-18 14:44:35 +0530

    Add files via upload

---
+++ nginx-node-mini-project/backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/greet', (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `Hello, ${name} from Server!` });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
